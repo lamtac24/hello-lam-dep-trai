c72a7bb574a086a6b6f01a060bec40582137e7da
Pham Lam <phamhonglam24082002@gmail.com>
2024-09-16 16:43:20 +0700

    lamnextgen

--- nextgen.java
+++ nextgen.java
@@ -2,5 +2,7 @@ public class nextgen {
     public static void main(String[]args) {
 		System.out.println("hi branch day");
 		System.out.println("toi cho them branch");
+
+        System.out.println("cai nay cua nhanh lamnextgen");
 	}
 }
